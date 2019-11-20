9d6398d7e5e95a33e413542523c49607ba8a89ef
VERDIER Thibaud <tverdier001@petrossian.pedago.ipb.fr>
2019-11-20 14:35:31 +0100

    Modify fortran

--- second_membre_sparse.f90
+++ second_membre_sparse.f90
@@ -1,81 +1,82 @@
 Module second_membre_sparse
 
-  Use fonction
-
-  Implicit None 
+  use fonctions
+  implicit none 
+  !  integer, parameter :: PR = 4
 
 Contains
 
-  Subroutine Mat(Nx,Ny,dx,dy,D,AA,JA,IA,dt)
-    Integer, Intent(in) :: Nx,Ny
-    Real(PR), Intent(in) :: dx,dy,D,dt
-    Real(PR), Dimension(:), Intent(out), Allocatable :: AA
-    Integer, Dimension(:), Intent(out), Allocatable :: JA,IA
+  subroutine Mat(Nx,Ny,dx,dy,D,AA,JA,IA,dt)
+    Integer, intent(in) :: Nx,Ny
+    Real(PR), intent(in) :: dx,dy,D,dt
+    Real(PR), dimension(:), intent(out), allocatable :: AA
+    integer, dimension(:), intent(out), allocatable :: JA,IA
     Integer :: i,j,k,nbNN
 
     nbNN = 5*(Nx*Ny) - 2*Nx - 2*Ny !! nombre d'éléments non nuls ?
-    Allocate(AA(nbNN),IA(nbNN),JA(nbNN))
+    allocate(AA(nbNN),IA(nbNN),JA(nbNN))
 
     k = 1
-    Do i=1,Nx*Ny
-       Do j=1,Ny*Ny
-          If(i==j) Then                           !! diagonale
+    do i=1,Nx*Ny
+       do j=1,Ny*Ny
+          if(i==j) then                           !! diagonale
              AA(k) = ((2/dx**2)+(2/dy**2))*D*dt + 1.   ! on code I+dt*A
              IA(k) = i
              JA(k) = j
              k = k + 1
-          Elseif((i==j-1) .And. (i/=Nx*Ny) .And. (Modulo(i,Nx)/=0)) Then  !diagonale sup
+          elseif((i==j-1) .AND. (i/=Nx*Ny) .AND. (modulo(i,Nx)/=0)) then!diagonale sup
              AA(k) = (-1/(dx**2))*D*dt
              IA(k) = i
              JA(k) = j
              k = k + 1
-          Elseif((i==j+1) .And. (i/=1) .And. (Modulo(j,Ny)/=0)) Then  !diagonale inf
+          elseif((i==j+1) .AND. (i/=1) .AND. (modulo(j,Ny)/=0)) then  !diagonale inf
+             AA(k) = (-1/(dy**2))*D*dt
+             IA(k) = i
+             JA(k) = j
+             k = k + 1
+          elseif(j==Ny+i) then                       ! diagonale tout en haut
+             AA(k) = (-1/(dy**2))*D*dt
+             IA(k) = i
+             JA(k) = j
+             k = k + 1
+          elseif(i==j+Nx) then                           !diagonale tout en bas
              AA(k) = (-1/(dx**2))*D*dt
              IA(k) = i
              JA(k) = j
              k = k + 1
-          Elseif(j==Ny+i) Then                       ! diagonale tout en haut
-             AA(k) = (-1/(dy**2))*D*dt
-             IA(k) = i
-             JA(k) = j
-             k = k + 1
-          Elseif(i==j+Ny) Then                           !diagonale tout en bas
-             AA(k) = (-1/(dy**2))*D*dt
-             IA(k) = i
-             JA(k) = j
-             k = k + 1
-          End If
-       End Do
-    End Do
-  End Subroutine Mat
+          end if
+       end do
+    end do
+  End subroutine Mat
 
-  Subroutine secondMembre(F,Nx,Ny,dx,dy,D,fsource,g,h)
-    Real(PR), Dimension(Nx*Ny), Intent(out) :: F
-    Real(PR), Dimension(Nx,Ny), Intent(in) :: fsource
-    Real(PR), Dimension(Nx,Ny), Intent(in) :: g
-    Real(PR), Dimension(Nx,Ny), Intent(in) :: h
-    Integer, Intent(in) :: Nx,Ny
-    Real(PR), Intent(in) :: dx,dy,D
+  subroutine secondMembre(F,Nx,Ny,dx,dy,D,fsource,g,h)
+    real(PR), dimension(Nx*Ny), intent(out) :: F
+    real(PR), dimension(Nx,Ny), intent(in) :: fsource
+    real(PR), dimension(Nx,Ny), intent(in) :: g
+    real(PR), dimension(Nx,Ny), intent(in) :: h
+    Integer, intent(in) :: Nx,Ny
+    Real(PR), intent(in) :: dx,dy,D
     Integer :: i,j,C
     C=1
-    Do i=1,Nx
-       Do j=1,Ny
+    do i=1,Nx
+       do j=1,Ny
           F(C) = fsource(i,j)
-          If ((i==1) .Or. (i==Nx)) Then
+          if ((i==1) .OR. (i==Nx)) then
              F(C) = F(C) + g(i,j)*D/dy**2
-             If ((j==1) .Or. (j==Ny)) Then
+             print*, g(i,j)
+             if ((j==1) .OR. (j==Ny)) then
                 F(C) = F(C) + h(i,j)*D/dx**2
-             End If
-          Elseif ((j==1) .Or. (j==Ny)) Then
+             end if
+          elseif ((j==1) .OR. (j==Ny)) then
              F(C) = F(C) + h(i,j)*D/dx**2
-             If ((i==1) .Or. (i==Nx)) Then
+             if ((i==1) .OR. (i==Nx)) then
                 F(C) = F(C) + g(i,j)*D/dy**2
-             End If
-          End If
+             end if
+          end if
           C = C + 1
-       End Do
-    End Do
+       end do
+    end do
 
-  End Subroutine secondMembre
+  end subroutine secondMembre
 
 End Module second_membre_sparse
