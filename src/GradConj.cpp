8a403e900b0208cac519346c7df01dfdf7c33474
Bridonneau Vincent <vincent.bridonneau@enseirb-matmeca.fr>
2019-12-04 16:19:41 +0100

    gradient conjugue + operation vector

--- src/GradConj.cpp
+++ src/GradConj.cpp
@@ -1,15 +1,43 @@
 #include "math/Sparse.h"
 #include "math/Vector.h"
+#include <cstdlib>
+#include <cmath>
 
-void GC_sparse_parallel(Sparse const* A,
-                        Vector const* b,
-                        Vector* x,
-                        Vector const* x0,
+void GC_sparse_parallel(Sparse const& A,
+                        Vector const& b,
+                        Vector& x,
+                        Vector const& x0,
                         int kmax,
                         double eps) {
-    Vector r = *b;
+    double alpha, beta, gamma;
+
+    Vector r = b;
+    Vector p = r;
     Vector prod;
-    if (x0)
-        prod = (*A) * (*x0);
+    if (x0.size() != 0)
+        prod = A * x0;
     r -= prod;
+
+    /* Computed here and updated at the end of the loop (avoid twice computing the same thing) */
+    double rdotr = r.dot(r);
+    for (size_t k = 0; k < kmax && beta > eps; ++k) {
+        gemv(1., A, prod);
+        alpha = rdotr / p.dot(prod);
+
+        /* x <- x + alpha * p */
+        axpy(alpha, p, x);
+        /* r <- r - alpha * A * p */
+        r -= prod.scale(alpha);
+
+        double rndotrn = r.dot(r);
+        /* gamma <- (rn * rn)/(r * r) */
+        gamma = rndotrn / rdotr;
+
+        /* r <- r + beta * p */
+        p.scale(gamma);
+        p += r;
+
+        beta = sqrt(rdotr);
+        rdotr = rndotrn;
+    }
 }
--- src/math/Sparse.cpp
+++ src/math/Sparse.cpp
@@ -4,4 +4,11 @@ Sparse::Sparse (double N, double alpha, double beta, double gamma) :
 N(N), alpha(alpha), beta(beta), gamma(gamma)
 {}
 
+Sparse::Sparse (Sparse const& A) : N(N), alpha(alpha), beta(beta), gamma(gamma)
+{}
+
 Sparse::~Sparse() {}
+
+void gemv(double a, Sparse const& A, Vector& x) {
+  
+}
