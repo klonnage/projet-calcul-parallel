665158c3858aacca574d725f3378603400c4866a
qborbeau <45460570+qborbeau@users.noreply.github.com>
2019-12-11 13:58:08 +0100

    Add files via upload

--- fonctions.f90
+++ fonctions.f90
@@ -1,219 +1,113 @@
 Module fonctions
 
-  !use mod_lecture
+  implicit none
 
-  implicit none 
+  include 'mpif.h'
 
   integer, parameter :: PR = 8
 
 Contains
 
-
-  subroutine vect_to_mat(u, u_mat, Nx, Ny)
-    real(PR), dimension(Nx*Ny), intent(in) :: u
-    real(PR), dimension(Nx,Ny), intent(out) :: u_mat
-    integer, intent(in) :: Nx, Ny
-    integer :: i, j, c
-    c=1
-    do i = 1, Nx
-       do j = 1, Ny
-          u_mat(i,j) = u(c)
-          c = c + 1
-       end do
-    end do
-  end subroutine vect_to_mat
-
-  subroutine lect_para(Nx, Ny, Lx, Ly, D, dt,tmax)
-    integer, intent(out) :: Nx, Ny
+  subroutine lect_para(Nli, Ncol, Lx, Ly, D, dt, tmax)
+    integer, intent(out) :: Nli, Ncol
     real(PR), intent(out) :: Lx, Ly, D, dt, tmax
 
     open(10, file="parametres.dat")
 
-    read(10, *) Nx
-    read(10, *) Ny
+    read(10, *) Nli
+    read(10, *) Ncol
     read(10, *) Lx
     read(10, *) Ly
-    read(10, *) D 
+    read(10, *) D
     read(10, *) dt
     read(10, *) tmax
+    read(10, *) mode
+    read(10, *) beta
+    read(10, *) kmax
 
     close(10)
 
   end subroutine lect_para
 
-  ! f,g et h  stationnaires
+  subroutine g(me, Ncol, dx, Ly, gme, mode)
+    integer, intent(in) :: me, Ncol, mode
+    real(PR), intent(in) :: dx, Ly
+    Real(PR), intent(in), dimension(2,Ncol) :: gme
+    integer :: i
+    real(PR) :: x
 
+    select case (mode)
+    case (1) ! conditions aux bords stationnaires
+      gme = 0
+    case (2) ! conditions aux bords periodiques
+      gme = 0
+      if (me = 0) then
+        do (i=1,Ncol)
+            x = real(i)*dx
+            gme(1,i) = sin(x) + 1 !cos(0) = 1
+        enddo
+      end if
+      if (me = Np) then
+        do (i=1,Ncol)
+            x = real(i)*dx
+            gme(2,i) = sin(x) + cos(Ly)
+        enddo
+      end if
+    case(3) ! conditions aux bords instationnaires periodiques
+      gme = 0
+    endselect
+  end subroutine g
 
-  function fsta(Lx,Ly,Nx,Ny)
-    Real(PR), Intent(in) :: Lx,Ly
-    integer, intent(in) :: Nx,Ny
-    Real(PR), dimension(Nx,Ny) :: fsta
-    integer :: i,j
-    real(PR) :: dx,dy,x,y
+  subroutine h(me, Nlime, i1, dy, Lx, hme, mode)
+    integer, intent(in) :: me, Nlime, i1, mode
+    real(PR), intent(in) :: dy, Lx
+    Real(PR), intent(in), dimension(Nlime,2) :: hme
+    integer :: i
 
-    dx = Lx / Nx 
-    dy = Ly / Ny
-    do i = 1,Nx
-       do j = 1,Ny
-          x = real(i)*dx
+    select case (mode)
+    case (1) ! conditions aux bords stationnaires
+      hme = 0
+    case (2) ! conditions aux bords periodiques
+      do (i=1,Nlime)
+          y = real(i + i1 - 1)*dy
+          hme(i,1) = cos(y) !sin(0) = 0
+          hme(i,2) = cos(y) + sin(Lx)
+      enddo
+    case(3) ! conditions aux bords instationnaires periodiques
+      hme = 1
+    endselect
+  end subroutine h
+
+subroutine fsource(me, Ncol, Nlime, i1, dx, dy, t, fsourceme, mode)
+    integer, intent(in) :: me, Ncol, Nlime, i1, mode
+    real(PR), intent(in) :: dy, Lx, t
+    Real(PR), intent(in), dimension(Nlime,Ncol) :: fsourceme
+    integer :: i, j
+
+    select case (mode)
+    case (1) ! terme source stationnaire
+      do i = 1,Nlime
+        do j = 1,Ncol
+          x = real(i + i1 -1)*dx
           y = real(j)*dy
-          fsta(i,j) = 2*(y - y**2 + x - x**2)
+          fsourceme(i,j) = 2*(y - y**2 + x - x**2)
+        end do
+      end do
+    case (2) ! terme source periodique
+      do i = 1,Nlime
+       do j = 1,Ncol
+          x = real(i + i1 - 1)*dx
+          y = real(j)*dy
+          fsourceme(i,j) = sin(x) + cos(y)
+       end do
+     end do
+   case(3) ! terme source instationnaires periodiques
+      do i = 1,Nlime
+       do j = 1,Ncol
+          x = real(i + i1 - 1)*dx
+          y = real(j)*dy
+          fsourceme(i,j) =  exp(-(x-Lx*0.5_PR)**2) * exp(-(y-Ly*0.5_PR)**2)*cos(t*pi*0.5_PR)
        end do
     end do
-  end function fsta
-
-
-  function gsta(Lx,Ly,Ny,Nx)
-    Real(PR), Intent(in) :: Ly,Lx
-    integer, intent(in) :: Ny,Nx
-    Real(PR), dimension(Nx,Ny) :: gsta
-    integer :: i,j
-    real(PR) :: dx,dy,x,y
-
-    dx = Lx / Nx 
-    dy = Ly / Ny
-    do i = 1,Nx
-       do j = 1,Ny
-          x = real(i)*dx
-          y = real(j)*dy
-          gsta(i,j) = 0
-       end do
-    end do
-  end function gsta
-
-  function hsta(Lx,Ly,Nx,Ny)
-    Real(PR), Intent(in) :: Lx,Ly
-    integer, intent(in) :: Nx,Ny
-    Real(PR), dimension(Nx,Ny) :: hsta
-    integer :: i,j
-    real(PR) :: dx,dy,x,y
-
-    dx = Lx / Nx 
-    dy = Ly / Ny
-    do i = 1,Nx
-       do j = 1,Ny
-          x = real(i)*dx
-          y = real(j)*dy
-          hsta(i,j) = 0
-       end do
-    end do
-
-  end function hsta
-
-
-  ! f,g et h periodiques
-
-  subroutine fper(Lx,Ly, Nx, Ny,A)
-    Real(PR), Intent(in) :: Lx,Ly
-    integer, intent(in) :: Nx,Ny
-    Real(PR), dimension(Nx,Ny), intent (inout) :: A
-    integer :: i,j
-    real(PR) :: dx,dy,x,y
-
-    dx = Lx / Nx 
-    dy = Ly / Ny
-    do i = 1,Nx
-       do j = 1,Ny
-          x = real(i)*dx
-          y = real(j)*dy
-          A(i,j) = sin(x) + cos(y)
-       end do
-    end do
-
-  end subroutine fper
-
-  function gper(Lx,Ly,Nx,Ny)
-    Real(PR), Intent(in) :: Ly,Lx
-    integer, intent(in) :: Ny,Nx
-    Real(PR), dimension(Nx,Ny) :: gper
-    integer :: i,j
-    real(PR) :: dx,dy,x,y
-
-    dx = Lx / Nx 
-    dy = Ly / Ny
-    do i = 1,Nx
-       do j = 1,Ny
-          x = real(i)*dx
-          y = real(j)*dy
-          gper(i,j) = sin(x) + cos(y)
-       end do
-    end do
-
-  end function gper
-
-  function hper(Lx,Ly,Nx,Ny)
-    Real(PR), Intent(in) :: Ly,Lx
-    integer, intent(in) :: Ny,Nx
-    Real(PR), dimension(Nx,Ny) :: hper
-    integer :: i,j
-    real(PR) :: dx,dy,x,y
-
-    dx = Lx / Nx 
-    dy = Ly / Ny
-    do i = 1,Nx
-       do j = 1,Ny
-          x = real(i)*dx
-          y = real(j)*dy
-          hper(i,j) = sin(x) + cos(y)
-       end do
-    end do
-  end function hper
-
-  ! f,g et h instationnaires periodiques
-
-  function finstaper(Lx,Ly,Nx,Ny,t) !appeler à chaque pas de temps
-    Real(PR), Intent(in) :: Ly,Lx,t
-    integer, intent(in) :: Ny,Nx
-    Real(PR), dimension(Nx,Ny) :: finstaper
-    integer :: i,j
-    real(PR) :: dx,dy,x,y
-
-    dx = Lx / Nx 
-    dy = Ly / Ny
-    do i = 1,Nx
-       do j = 1,Ny
-          x = real(i)*dx
-          y = real(j)*dy
-          finstaper(i,j) =  exp(-(x-(Lx)/2)**2) * exp(-(y-(Ly)/2)**2)*cos((2*acos(-1.)/2)*t)
-       end do
-    end do
-
-  end function finstaper
-
-  function ginstaper(Lx,Ly,Nx,Ny)
-    Real(PR), Intent(in) :: Ly,Lx
-    integer, intent(in) :: Ny,Nx
-    Real(PR), dimension(Nx,Ny) :: ginstaper
-    integer :: i,j
-    real(PR) :: dx,dy,x,y
-
-    dx = Lx / Nx 
-    dy = Ly / Ny
-    do i = 1,Nx
-       do j = 1,Ny
-          x = real(i)*dx
-          y = real(j)*dy
-          ginstaper(i,j) = 0
-       end do
-    end do
-  end function ginstaper
-
-  function hinstaper(Lx,Ly,Nx,Ny)
-    Real(PR), Intent(in) :: Ly,Lx
-    integer, intent(in) :: Ny,Nx
-    Real(PR), dimension(Nx,Ny) :: hinstaper
-    integer :: i,j
-    real(PR) :: dx,dy,x,y
-
-    dx = Lx / Nx 
-    dy = Ly / Ny
-    do i = 1,Nx
-       do j = 1,Ny
-          x = real(i)*dx
-          y = real(j)*dy
-          hinstaper(i,j) = 1
-       end do
-    end do
-  end function hinstaper
-
-end Module fonctions
+   endselect
+ end subroutine fsource
